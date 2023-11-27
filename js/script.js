8072aa0932878d934a55a8d0587c2dc6e23284e0
KostasKyriakosBtaios <konstantinos.mpatsios@gmail.com>
2023-11-27 20:19:24 +0200

    developed further the index, style, and script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-function toggleMode() {
-    const body = document.body;
-    body.classList.toggle("light-mode");
-    body.classList.toggle("dark-mode");
-}
+const sidebarMenu = document.querySelector("aside");
+const menuButton = document.querySelector("#menu-btn");
+const closeButton = document.querySelector("#close-btn");
+
+menuButton.addEventListener('click', () => {
+    sidebarMenu.style.display = 'block';
+});
+
+closeButton.addEventListener('click', () => {
+    sidebarMenu.style.display = 'none';
+});
