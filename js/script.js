35a7a0da1a0d960a1e8e4a4c6a1572bafc527fe0
KostasKyriakosBtaios <konstantinos.mpatsios@gmail.com>
2023-12-04 20:10:42 +0200

    new logo, theme toggle complete, and some formatting of the code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
-const sideMenu = document.querySelector("aside");
-const menuBtn = document.querySelector("#menu-button");
-const closeBtn = document.querySelector("#close-button");
+const sideMenu = document.querySelector('aside');
+const menuBtn = document.querySelector('#menu-button');
+const closeBtn = document.querySelector('#close-button');
+const themeToggler = document.querySelector('.theme-toggler');
 
+// show sidebar
 menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'block';
 });
 
+// close sidebar
 closeBtn.addEventListener('click', () => {
     sideMenu.style.display = 'none';
+});
+
+// change theme
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables');
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
 });
