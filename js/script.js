9b693839525b2164097918550ef51c9bd6403f21
KostasKyriakosBtaios <konstantinos.mpatsios@gmail.com>
2023-12-12 09:20:41 +0200

    changed index.html, style.css, script.js, and deleted signin.html, signin.js and profile.png

--- js/script.js
+++ js/script.js
@@ -1,50 +1,11 @@
-const themeToggler = document.querySelector('.theme-toggler');
+const wrapper = document.querySelector('.wrapper');
+const loginBtn = document.querySelector('.login-btn');
+const closeIcon = document.querySelector('.icon-close')
 
-// refresh the page
-function refreshWelcomePage() {
-    window.location.href = 'index.html';
-}
-
-// change theme
-themeToggler.addEventListener('click', () => {
-    document.body.classList.toggle('dark-theme-variables');
-    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
-    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+loginBtn.addEventListener('click', () => {
+    wrapper.classList.add('active');
 });
 
-// pressing the button sign in and go to the sign in form
-function signIn() {
-    window.location.href = "signin.html";
-}
-
-// go to the 1st chapter
-document.getElementById("first").addEventListener("click", function(event) {
-    event.preventDefault(); // Prevents the default behavior of the link
-    document.getElementById("chapter1").scrollIntoView({
-        behavior: "smooth"
-    });
-});
-
-// go to the 2nd chapter
-document.getElementById("second").addEventListener("click", function(event) {
-    event.preventDefault(); // Prevents the default behavior of the link
-    document.getElementById("chapter2").scrollIntoView({
-        behavior: "smooth"
-    });
-});
-
-// go to the 3rd chapter
-document.getElementById("third").addEventListener("click", function(event) {
-    event.preventDefault(); // Prevents the default behavior of the link
-    document.getElementById("chapter3").scrollIntoView({
-        behavior: "smooth"
-    });
-});
-
-// go to the 4th chapter
-document.getElementById("fourth").addEventListener("click", function(event) {
-    event.preventDefault(); // Prevents the default behavior of the link
-    document.getElementById("chapter4").scrollIntoView({
-        behavior: "smooth"
-    });
+closeIcon.addEventListener('click', () => {
+    wrapper.classList.remove('active');
 });
