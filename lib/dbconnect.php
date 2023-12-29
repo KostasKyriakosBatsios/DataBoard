581ac4815a64ec18eeae466358a4706db11be965
Alexandros Oikonomou <alexoik2001@gmail.com>
2023-12-29 13:40:20 +0200

    Corrected total view intervals calculation of final_records table.

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -12,6 +12,7 @@
         echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     }
 
+    require_once "db_upass.php";
     // Creating and Inserting the final data into final_records table of DB.
     // Steps: 1) Using TRUNCATE to clean the data from final_records table.
     //        2) Inserting the final data into final_records table with the calculated total_view_time (based on view_records table).
@@ -20,25 +21,9 @@
     $mysqli->query($cleaning_data);
 
     $view_time_calc = "INSERT INTO final_records (user_id, video_id, total_view_time)
-                       WITH WatchIntervals AS (
-                            SELECT
-                                user,
-                                video,
-                                MIN(begin) AS start_time,
-                                MAX(end) AS end_time
-                            FROM
-                                view_records
-                            GROUP BY
-                                user, video
-                        )
-                        SELECT
-                            user,
-                            video,
-                            SUM(end_time - start_time + 1)
-                        FROM
-                            WatchIntervals
-                        GROUP BY
-                            user, video";
+                       SELECT user, video, SUM(end - begin) AS total_view_intervals
+                       FROM view_records
+					   GROUP BY user, video;";
     $mysqli->query($view_time_calc);    
     
     $final_data = "UPDATE final_records
