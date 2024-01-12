3cc13f818ad621e49e467ecc1da2c46141946800
KostasKyriakosBtaios <konstantinos.mpatsios@gmail.com>
2024-01-12 18:10:42 +0200

    added image to the databoard.php 'header'

--- databoard.php
+++ databoard.php
@@ -18,6 +18,7 @@
         <!-- Responsive -->
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>DataBoard</title>
+        <link rel="shortcut icon" href="img/DataBoard.png" type="image/">
         <!-- jQuery Library -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
         <!-- CSS Files & Icons -->
