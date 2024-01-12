991ee443326d474633041773656250c0069c49b1
KostasKyriakosBtaios <konstantinos.mpatsios@gmail.com>
2024-01-12 18:07:07 +0200

    testing the logout operation

--- lib/logout.php
+++ lib/logout.php
@@ -2,6 +2,6 @@
     // loging out and redirecting to the home page
     session_start();
     session_destroy();
-    header("Location: index.php");
+    header("Location: ../index.php");
     exit();
 ?>
